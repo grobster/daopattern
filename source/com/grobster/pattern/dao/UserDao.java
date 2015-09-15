163d17b19e855c315e6862f5f9e1c1ac195e9e0b
grobster <jquarles@grobster.com>
2015-09-15 14:05:35 -0400

    created User class and DAO interface

---
+++ source/com/grobster/pattern/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.grobster.pattern.dao;
+
+import java.util.*;
+
+public interface UserDao {
+	public int create(User u);
+	public User get(String lastName);
+	public boolean delete(User u);
+	public boolean update(User u);
+	public List<User> getAll();
+}
